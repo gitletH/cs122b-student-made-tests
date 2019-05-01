e4ecb079a8fdd51737ae3ee143fd8783dcad4b37
taylorfutral <taylorfutral@dhcp-v028-151.mobile.uci.edu>
2019-04-30 20:11:52 -0700

    edited script

--- hw4/hw4_tables_test.sql
+++ hw4/hw4_tables_test.sql
@@ -51,12 +51,12 @@ VALUES
 ('sample@sample.com', 't012345678', 5),
 ('sample@sample.com', 't012345678', 5);
 
-# FOREIGN KEY CONSTRAIN ON ccId references creditcards.id
+# FOREIGN KEY CONSTRAINT ON ccId references creditcards.id
 INSERT INTO customers (email, firstName, lastName, ccId, address)
 VALUES
-    ('amcheng@uci.edu', 'Alex', 'Cheng', 'not_a_cc_in_creditcards', 'throw_away_address');
+    ('another_email@uci.edu', 'Another', 'Guy', 'not_a_cc_in_table', 'throw_away_address');
 
-# FOREIGN KEY CONSTRAIN ON email references customers.email
+# FOREIGN KEY CONSTRAINT ON email references customers.email
 INSERT INTO sales (email, movieId, quantity, saleDate)
 VALUES
     ('not_an_email_in_customers@uci.edu', 't234121531', 2, '2019-04-10');
